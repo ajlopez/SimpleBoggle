993ffa8a19bc94e59c1dbec2a49446a007274142
ajlopez <webmaster@ajlopez.com>
2017-11-18 12:06:08 -0300

    Test find word using simpleunit

--- test/find.js
+++ test/find.js
@@ -1,60 +1,58 @@
 
-var simpleboggle = require('../'),
-    assert = require('assert');
+var simpleboggle = require('../');
 
-// createBoard defined
+exports['find letter'] = function (test) {
+	var board = simpleboggle.createBoard();
 
-var board = simpleboggle.createBoard();
+	board.setLetter(2, 2, 'A');
+	board.setLetter(2, 3, 'B');
+	board.setLetter(3, 3, 'B');
 
-// Find letter
+	var cells = board.findLetter('Z');
+	test.ok(cells);
+	test.equal(cells.length, 0);
+	var cells = board.findLetter('A');
+	test.ok(cells);
+	test.equal(cells.length, 1);
+	cells.forEach(function (cell) { test.equal(cell.letter, 'A'); });
+	var cells = board.findLetter('B');
+	test.ok(cells);
+	test.equal(cells.length, 2);
+	cells.forEach(function (cell) { test.equal(cell.letter, 'B'); });
+}
 
-board.setLetter(2, 2, 'A');
-board.setLetter(2, 3, 'B');
-board.setLetter(3, 3, 'B');
+exports['find word'] = function (test) {
+	var board = simpleboggle.createBoard();
+	board.setLetter(0, 0, 'S');
+	board.setLetter(0, 1, 'P');
+	board.setLetter(0, 2, 'A');
+	board.setLetter(0, 3, 'M');
 
-var cells = board.findLetter('Z');
-assert.ok(cells);
-assert.equal(cells.length, 0);
-var cells = board.findLetter('A');
-assert.ok(cells);
-assert.equal(cells.length, 1);
-cells.forEach(function (cell) { assert.equal(cell.letter, 'A'); });
-var cells = board.findLetter('B');
-assert.ok(cells);
-assert.equal(cells.length, 2);
-cells.forEach(function (cell) { assert.equal(cell.letter, 'B'); });
+	var results = board.findWord('PIPE');
+	test.ok(results);
+	test.equal(results.length, 0);
 
-// Find word
+	var results = board.findWord('SPAM');
+	test.ok(results);
+	test.equal(results.length, 1);
+	test.equal(results[0][0].x, 0);
+	test.equal(results[0][0].y, 0);
 
-var board = simpleboggle.createBoard();
-board.setLetter(0, 0, 'S');
-board.setLetter(0, 1, 'P');
-board.setLetter(0, 2, 'A');
-board.setLetter(0, 3, 'M');
+	var board = simpleboggle.createBoard();
+	board.setLetter(0, 0, 'P');
+	board.setLetter(0, 1, 'A');
+	board.setLetter(0, 2, 'P');
+	board.setLetter(0, 3, 'A');
 
-var results = board.findWord('PIPE');
-assert.ok(results);
-assert.equal(results.length, 0);
+	var results = board.findWord('PA');
+	test.ok(results);
+	test.equal(results.length, 3);
 
-var results = board.findWord('SPAM');
-assert.ok(results);
-assert.equal(results.length, 1);
-assert.equal(results[0][0].x, 0);
-assert.equal(results[0][0].y, 0);
+	board.setLetter(1, 0, 'A');
+	board.setLetter(1, 1, 'P');
 
-var board = simpleboggle.createBoard();
-board.setLetter(0, 0, 'P');
-board.setLetter(0, 1, 'A');
-board.setLetter(0, 2, 'P');
-board.setLetter(0, 3, 'A');
+	var results = board.findWord('PA');
+	test.ok(results);
+	test.equal(results.length, 6);	
+}
 
-var results = board.findWord('PA');
-assert.ok(results);
-assert.equal(results.length, 3);
-
-board.setLetter(1, 0, 'A');
-board.setLetter(1, 1, 'P');
-
-var results = board.findWord('PA');
-assert.ok(results);
-assert.equal(results.length, 6);
