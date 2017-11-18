11f1754b1205d481f1d8bfd75f6df9d50644bfd4
ajlopez <webmaster@ajlopez.com>
2017-11-18 11:54:12 -0300

    Test board using simpleunit

--- test/board.js
+++ test/board.js
@@ -1,64 +1,68 @@
 
-var simpleboggle = require('../'),
-    assert = require('assert');
+var simpleboggle = require('../');
 
-// createBoard defined
+exports['createBoard defined'] = function (test) {
+	test.ok(simpleboggle.createBoard);
+	test.equal(typeof simpleboggle.createBoard, 'function');
+}
 
-assert.ok(simpleboggle.createBoard);
-assert.equal(typeof simpleboggle.createBoard, 'function');
+exports['create board'] = function (test) {
+	var board = simpleboggle.createBoard();
+	test.ok(board);
+	test.equal(board.getWidth(), 4);
+	test.equal(board.getHeight(), 4);
+}
 
-// Create Board
+exports['get cell'] = function (test) {
+	var board = simpleboggle.createBoard();
 
-var board = simpleboggle.createBoard();
-assert.ok(board);
-assert.equal(board.getWidth(), 4);
-assert.equal(board.getHeight(), 4);
+	var cell = board.getCell(0, 0);
+	test.ok(cell);
+	test.equal(cell.x, 0);
+	test.equal(cell.y, 0);
+	test.equal(cell.letter, undefined);
+	test.ok(cell.neighbors);
+	test.equal(cell.neighbors.length, 3);
 
-// Get cell
+	var cell = board.getCell(1, 1);
+	test.ok(cell);
+	test.equal(cell.x, 1);
+	test.equal(cell.y, 1);
+	test.equal(cell.letter, undefined);
+	test.ok(cell.neighbors);
+	test.equal(cell.neighbors.length, 8);
 
-var cell = board.getCell(0, 0);
-assert.ok(cell);
-assert.equal(cell.x, 0);
-assert.equal(cell.y, 0);
-assert.equal(cell.letter, undefined);
-assert.ok(cell.neighbors);
-assert.equal(cell.neighbors.length, 3);
+	var cell = board.getCell(0, 1);
+	test.ok(cell);
+	test.equal(cell.letter, undefined);
+	test.ok(cell.neighbors);
+	test.equal(cell.neighbors.length, 5);
 
-var cell = board.getCell(1, 1);
-assert.ok(cell);
-assert.equal(cell.x, 1);
-assert.equal(cell.y, 1);
-assert.equal(cell.letter, undefined);
-assert.ok(cell.neighbors);
-assert.equal(cell.neighbors.length, 8);
+	var cell = board.getCell(0, 2);
+	test.ok(cell);
+	test.equal(cell.letter, undefined);
+	test.ok(cell.neighbors);
+	test.equal(cell.neighbors.length, 5);
 
-var cell = board.getCell(0, 1);
-assert.ok(cell);
-assert.equal(cell.letter, undefined);
-assert.ok(cell.neighbors);
-assert.equal(cell.neighbors.length, 5);
+	var cell = board.getCell(0, 3);
+	test.ok(cell);
+	test.equal(cell.letter, undefined);
+	test.ok(cell.neighbors);
+	test.equal(cell.neighbors.length, 3);	
+}
 
-var cell = board.getCell(0, 2);
-assert.ok(cell);
-assert.equal(cell.letter, undefined);
-assert.ok(cell.neighbors);
-assert.equal(cell.neighbors.length, 5);
+exports['set letter'] = function (test) {
+	var board = simpleboggle.createBoard();
 
-var cell = board.getCell(0, 3);
-assert.ok(cell);
-assert.equal(cell.letter, undefined);
-assert.ok(cell.neighbors);
-assert.equal(cell.neighbors.length, 3);
+	board.setLetter(2, 2, 'A');
+	var cell = board.getCell(2, 2);
+	test.equal(cell.letter, 'A');
+}
+	
+exports['create board with other size'] = function (test) {
+	var board = simpleboggle.createBoard(2, 3);
+	test.ok(board);
+	test.equal(board.getWidth(), 2);
+	test.equal(board.getHeight(), 3);
+}
 
-// Set letter
-
-board.setLetter(2, 2, 'A');
-var cell = board.getCell(2, 2);
-assert.equal(cell.letter, 'A');
-
-// Create board with other size
-
-var board = simpleboggle.createBoard(2, 3);
-assert.ok(board);
-assert.equal(board.getWidth(), 2);
-assert.equal(board.getHeight(), 3);
