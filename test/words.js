c9dabdde986b435705bdc62edd4a9c9df83eb416
ajlopez <webmaster@ajlopez.com>
2017-11-18 14:01:52 -0300

    Test words using simpleunit

--- test/words.js
+++ test/words.js
@@ -1,57 +1,60 @@
 
-var simpleboggle = require('../'),
-    assert = require('assert');
+var simpleboggle = require('../');
 
-// build Word Tree
+exports['build word tree'] = function (test) {
+	test.ok(simpleboggle.buildWordTree);
+	test.equal(typeof simpleboggle.buildWordTree, 'function');
+}
 
-assert.ok(simpleboggle.buildWordTree);
-assert.equal(typeof simpleboggle.buildWordTree, 'function');
+exports['one word'] = function (test) {
+	var tree = simpleboggle.buildWordTree(['spam']);
+	test.ok(tree);
+	test.ok(tree.s);
+	test.ok(tree.s.p);
+	test.ok(tree.s.p.a);
+	test.ok(tree.s.p.a.m);
+	test.ok(tree.s.p.a.m.$ !== undefined);
+}
 
-// One word
+exports['two words'] = function (test) {
+	var tree = simpleboggle.buildWordTree(['spam', 'spice']);
 
-var tree = simpleboggle.buildWordTree(['spam']);
-assert.ok(tree);
-assert.ok(tree.s);
-assert.ok(tree.s.p);
-assert.ok(tree.s.p.a);
-assert.ok(tree.s.p.a.m);
-assert.ok(tree.s.p.a.m.$ !== undefined);
+	test.ok(tree);
+	test.ok(tree.s);
+	test.ok(tree.s.p);
+	test.ok(tree.s.p.a);
+	test.ok(tree.s.p.a.m);
+	test.ok(tree.s.p.a.m.$ !== undefined);
+	test.ok(tree.s.p.i);
+	test.ok(tree.s.p.i.c);
+	test.ok(tree.s.p.i.c.e);
+	test.ok(tree.s.p.i.c.e.$ !== undefined);
+}
 
-// Two words
+exports['find word'] = function (test) {
+	var board = simpleboggle.createBoard();
+	var tree = simpleboggle.buildWordTree(['spam', 'spice']);
 
-var tree = simpleboggle.buildWordTree(['spam', 'spice']);
-assert.ok(tree);
-assert.ok(tree.s);
-assert.ok(tree.s.p);
-assert.ok(tree.s.p.a);
-assert.ok(tree.s.p.a.m);
-assert.ok(tree.s.p.a.m.$ !== undefined);
-assert.ok(tree.s.p.i);
-assert.ok(tree.s.p.i.c);
-assert.ok(tree.s.p.i.c.e);
-assert.ok(tree.s.p.i.c.e.$ !== undefined);
+	board.setLetter(0, 0, 's');
+	board.setLetter(0, 1, 'p');
+	board.setLetter(0, 2, 'a');
+	board.setLetter(0, 3, 'm');
 
-// Find word
+	var results = board.findWords(tree);
+	test.ok(results);
+	test.equal(results.length, 1);
 
-var board = simpleboggle.createBoard();
-board.setLetter(0, 0, 's');
-board.setLetter(0, 1, 'p');
-board.setLetter(0, 2, 'a');
-board.setLetter(0, 3, 'm');
+	board.setLetter(1, 1, 'i');
+	board.setLetter(2, 1, 'c');
+	board.setLetter(3, 1, 'e');
 
-var results = board.findWords(tree);
-assert.ok(results);
-assert.equal(results.length, 1);
+	var results = board.findWords(tree);
+	test.ok(results);
+	test.equal(results.length, 2);
 
-board.setLetter(1, 1, 'i');
-board.setLetter(2, 1, 'c');
-board.setLetter(3, 1, 'e');
+	var word = simpleboggle.cellsToWord(results[0]);
+	test.ok(word === 'spam' || word === 'spice');
+	var word = simpleboggle.cellsToWord(results[1]);
+	test.ok(word === 'spam' || word === 'spice');
+}
 
-var results = board.findWords(tree);
-assert.ok(results);
-assert.equal(results.length, 2);
-
-var word = simpleboggle.cellsToWord(results[0]);
-assert.ok(word === 'spam' || word === 'spice');
-var word = simpleboggle.cellsToWord(results[1]);
-assert.ok(word === 'spam' || word === 'spice');
